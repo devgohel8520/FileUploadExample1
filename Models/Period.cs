881839cb266955797649902e7fcca2bb3397889e
devgohel8520 <lectroondev@gmail.com>
2016-04-08 23:43:34 +0530

    FileUpload
    Complete source code of file upload in mvc

---
+++ Models/Period.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadFileMVC.Models
+{
+    public class Period
+    {
+        public DateTime submission_deadline_date { get; set; }
+        public string period_covered { get; set; }
+        public HttpPostedFileBase File { get; set; }
+        //public string fk_level_register { get; set; }
+        //public DateTime date_modified { get; set; }
+        //public string done_by { get; set; }
+    }
+}
